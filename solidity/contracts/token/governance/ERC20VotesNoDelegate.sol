ed074e60f6fe5d94d3bb519d245b9499d4c7f2bb
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2022-07-02 16:11:46 +0800

    Update ERC20VotesNoDelegate.sol

--- solidity/contracts/token/governance/ERC20VotesNoDelegate.sol
+++ solidity/contracts/token/governance/ERC20VotesNoDelegate.sol
@@ -28,47 +28,68 @@ import "./ERC20CarbonDAO.sol";
 abstract contract ERC20VotesNoDelegate is ERC20CarbonDAO, IVotes {
 
     /**
-    * @dev The Vote structure is like a ballot
-    * @param addr is the address of the voter
-    * @param amount is the voting power of the voter
-    * @param option is the option of the voter
+    * @dev The Vote structure records the voting infomation of a proposal
+    * @param voted is a mapping from an address to its voting information
+    * @param agree_amount is the voted amount for "agree"
+    * @param disagree_amount is the voted amount for "disagree"
     */
     struct Vote {
-        address addr;
-        uint256 amount;
-        //uint128 proposal;
-        uint8 option; 
+        mapping(address => int256) voted;
+        uint128 agree_amount;
+        uint128 disagree_amount;
     }
 
     /**
-    * @dev All votes
-    * The first mapping maps a proposal id to the second mapping
-    * The second mapping maps an address to its ballot
-    */
-    mapping(bytes32 => mapping(address => Vote)) public votes;
-
-    /**
     * @dev All proposals
+    * The mapping maps a proposal hash to a Vote structure
     */
-    mapping(bytes32 => bool) public proposals;
+    mapping(bytes32 => Vote) public proposals;
 
     /**
     * @dev Vote for a proposal
-    * @param proposal is the proposal id
-    * @param option is the option to vote (e.g. endorse, oppose, etc.)
+    * @param proposal is the proposal hash
+    * @param option is the option to vote (agree / disagree)
     */
-    function vote(bytes32 proposal, uint8 option) public {
+    function vote(bytes32 proposal, bool option) public {
         require(uint32(proposal) > block.timestamp); // Proposal is valid for voting
-        uint256 voting_power = getVotes(msg.sender);
-        Vote v(msg.sender, voting_power, option);
-        votes[proposal][msg.sender] = v;
+        uint256 voting_amount = getVotes(msg.sender);
+        //Vote v(msg.sender, voting_power, option);
+        //votes[proposal][msg.sender] = v;
+        int256 last_voted_amount = proposals[proposal].voted[msg.sender];
+
+        // Wipe out the last vote of the sender
+        if (last_voted_amount > 0) {
+            proposals[proposal].agree_amount -= last_voted_amount;
+        } else if (last_voted_amount < 0) {
+            proposals[proposal].disagree_amount += last_voted_amount;
+        }
+
+        // Add the new vote
+        if (option) {
+            proposals[proposal].agree_amount += voting_amount;
+        } else {
+            proposals[proposal].disagree_amount += voting_amount;
+        }
+
+    }
+
+    /**
+    * @dev Gets the current voting amount for `account`
+    */
+    function sqrt(uint x) public pure returns (uint y) {
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
     }
 
     /**
     * @dev Gets the current votes balance for `account`
     */
     function getVotes(address account) public view virtual override returns (uint256) {
-        return balanceOf(account);
+        return sqrt(balanceOf(account));
     }
 
     /**
@@ -78,7 +99,9 @@ abstract contract ERC20VotesNoDelegate is ERC20CarbonDAO, IVotes {
     * where url is the Arweave url.
     */
     function addProposal(bytes32 proposal) public {
-        require(proposals[proposal] == false, "Proposal already exists.");
-        proposals[proposal] = true;
+        require(proposals[proposal].agree_amount == 0 && proposals[proposal].disagree_amount == 0, "Proposal already exists.");
+        Vote storage v = proposals[proposal];
+        v.voted[msg.sender] = getVotes(msg.sender);
+        v.agree_amount = getVotes(msg.sender);
     }
 }
